a8c5376c740761b93156402ebbb4e68ffc406d5b
Tom Craven <tcraven@cardinalpeak.com>
2023-10-23 13:44:05 -0600

    Website text update and build.

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -13,16 +13,19 @@ export const Tech = () => {
 
             <h2>Overview</h2>
             <p>The Cat Detector is an IoT application that uses machine learning to
-                detect cats in a Raspberry Pi camera. It consists of two components - an
+                detect cats in a Raspberry Pi camera. Potential use cases include
+                keeping your cats off the kitchen counter or away from houseplants.
+                The application consists of two components - an
                 IoT application running on the Pi, and a website hosted in the cloud.</p>
 
             <h3>Raspberry Pi IoT Application</h3>
-            <p>The Cat Detector is an AWS IoT Greengrass application that runs continuously
+            <p>The Cat Detector is an <a href="https://docs.aws.amazon.com/greengrass/v2/developerguide/what-is-iot-greengrass.html" target="_blank" rel="noreferrer">AWS IoT Greengrass</a> application
+                that runs continuously
                 on the Pi as a set of background processes. It has the following
                 components:</p>
 
             <h4>Greengrass Nucleus</h4>
-            <p>The main controller for the other components. This is a standard AWS Greengrass
+            <p>The main controller for the other components is <a href="https://docs.aws.amazon.com/greengrass/v2/developerguide/greengrass-nucleus-component.html" target="_blank" rel="noreferrer">AWS IoT Greengrass Nucleus</a>. This is a standard AWS IoT Greengrass
                 component that handles starting and stopping the other components,
                 configuration, logging, component software updates, etc.</p>
 
@@ -46,39 +49,38 @@ export const Tech = () => {
 
             <h4>WebRTC Client</h4>
             <p>The WebRTC Client component accepts WebRTC calls from the Website video player
-                via the AWS Kinesis video service. When a call is started, it starts its own
+                via the <a href="https://aws.amazon.com/kinesis/video-streams/" target="_blank" rel="noreferrer">AWS Kinesis video service</a>.
+                When a call is started, it starts its own
                 GStreamer pipeline using the Video Source as its source, and converts the
-                video into the required H.264 format, using hardware acceleration.</p>
+                video into the required H.264 format using hardware acceleration.</p>
             <p>The component is a C application, based on an example application from
-                the AWS Kinesis WebRTC C SDK. When a call ends, the component releases its
+                the <a href="https://docs.aws.amazon.com/kinesisvideostreams-webrtc-dg/latest/devguide/kvswebrtc-sdk-c.html" target="_blank" rel="noreferrer">AWS Kinesis WebRTC C SDK</a>.
+                When a call ends, the component releases its
                 resources so that it is ready to accept the next call.</p>
 
             <h4>Object Detector</h4>
             <p>This component is responsible for detecting cats in the video. It is a Python
                 application that reads RGB video frames from the second Video Source stream
                 once per second. Frames are classified using a machine learning model,
-                and if a cat is detected, a message is published to an IoT MQTT topic and to
-                an internal Greengrass IPC topic. These messages are picked up by the Website
+                and if a cat is detected, a message is published to an <a href="https://docs.aws.amazon.com/iot/latest/developerguide/topics.html" target="_blank" rel="noreferrer">IoT MQTT topic</a> and to
+                an internal <a href="https://docs.aws.amazon.com/greengrass/v2/developerguide/ipc-publish-subscribe.html" target="_blank" rel="noreferrer">Greengrass IPC topic</a>. These messages are picked up by the Website
                 and Audio Player component respectively.</p>
-            <p>The machine learning model used is a pre-trained ResNet50 model running on the
-                PyTorch Torchvision library. Each classification takes approximately 2.5
+            <p>The machine learning model used is a pre-trained ResNet50 model running on the <a href="https://pytorch.org/vision/stable/index.html" target="_blank" rel="noreferrer">PyTorch Torchvision</a> library. Each classification takes approximately 2.5
                 seconds to run on the Pi, and only one classification runs at a time. This
-                means that one in every three video frames is classified, and there there is a
+                means that one in every three video frames is classified, and there is a
                 delay of approximately five seconds between the cat being put in the Pi camera
-                view, and the publish of the detection message.</p>
+                view, and the publishing of the detection message.</p>
 
             <h4>Audio Player</h4>
             <p>The Audio Player component is responsible for playing a short audio clip each
                 time a cat is detected. It subscribes to a local Greengrass IPC topic, and when
                 a cat detection message is received, it plays a WAV file through a speaker
-                plugged in to the Pi headphones jack.</p>
+                plugged into the Pi headphones jack.</p>
 
             <h3>Cloud Components</h3>
 
             <h4>Website</h4>
-            <p>The website is a static web application written in React. It uses the AWS
-                Amplify SDK and AWS Kinesis WebRTC SDK libraries, and is hosted via Github
-                Pages.</p>
+            <p>The website is a static web application written in React. It uses the <a href="https://aws.amazon.com/amplify/" target="_blank" rel="noreferrer">AWS Amplify SDK</a> and <a href="https://docs.aws.amazon.com/kinesisvideostreams-webrtc-dg/latest/devguide/kvswebrtc-sdk-js.html" target="_blank" rel="noreferrer">AWS Kinesis WebRTC SDK</a> libraries, and is hosted via Github Pages.</p>
             <p>It has four main features:</p>
             <ul>
                 <li>
@@ -94,7 +96,7 @@ export const Tech = () => {
                         received by the web page</p>
                 </li>
                 <li>
-                    <p>Users are authenticated using AWS Cognito, and must sign in via a Cognito
+                    <p>Users are authenticated using <a href="https://aws.amazon.com/pm/cognito" target="_blank" rel="noreferrer">AWS Cognito</a>, and must sign in via a Cognito
                         hosted login page. Once signed in, the Cognito session provides the necessary
                         AWS credentials for the web page to authenticate to the AWS IoT and AWS Kinesis
                         WebRTC services.</p>
@@ -102,14 +104,14 @@ export const Tech = () => {
                 <li>
                     <p>An information section displays instructions how to use the demo,
                         technical information about the architecture of the Cat Detector application,
-                        and information about Cardinal Peak</p>
+                        and information about AWS IoT Consulting Partner Cardinal Peak</p>
                 </li>
             </ul>
             
             <h4>AWS Greengrass</h4>
             <p>The initial set-up and configuration of the Pi, and subsequent component
                 software updates are performed manually using a combination of the AWS
-                Console and running local Greengrass CLI commands.</p>
+                Console and running local <a href="https://docs.aws.amazon.com/greengrass/v2/developerguide/gg-cli-reference.html" target="_blank" rel="noreferrer">Greengrass CLI commands</a>.</p>
 
             <h4>AWS Kinesis Video Signaling Channels</h4>
             <p>This service is used for the WebRTC video calls, but no manual configuration
@@ -117,7 +119,7 @@ export const Tech = () => {
                 and development.</p>
 
             <h4>AWS Cognito</h4>
-            <p>User authentication is handled AWS Cognito. A User Pool is configured to
+            <p>User authentication is handled by AWS Cognito. A User Pool is configured to
                 provide a hosted login page for the application. A corresponding Identity Pool
                 is used to authorize Cognito sessions to access the necessary resources in AWS
                 IoT and AWS Kinesis Video services.</p>
